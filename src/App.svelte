d5fccdae58ff621df64e09a604f8eb9f950e1115
Gadzhi Kharkharov <me@kkga.me>
2021-02-21 19:42:41 +0300

    play around

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,36 @@
 <script lang="ts">
+	import Input from "./Input.svelte";
+
 	export let name: string;
+	let color: string;
+	let size: number;
+
+	$: console.log(color);
+	$: console.log(size);
+
+	const properties = [
+		{
+			prop: "color",
+			label: "Color",
+			type: "enum",
+			values: ["red", "yellow", "green"],
+		},
+		{
+			prop: "size",
+			label: "Size",
+			type: "number",
+		},
+	];
+
+	function handleChange(e) {
+		let name: string = e.detail.name;
+		let value: any = e.detail.value;
+		if (name == "color") {
+			color = value;
+		} else if (name == "size") {
+			size = value;
+		}
+	}
 </script>
 
 <style>
@@ -11,7 +42,6 @@
 	}
 
 	h1 {
-		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
@@ -25,10 +55,18 @@
 </style>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>
-		Visit the
-		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-		to learn how to build Svelte apps.
-	</p>
+	<h1 style="color: {color}; font-size: {size}px">{name}</h1>
+
+	{#each properties as { prop, label, type, values }}
+		{#if type == 'enum'}
+			<Input
+				name={prop}
+				{label}
+				type="select"
+				on:change={handleChange}
+				{values} />
+		{:else if type == 'number'}
+			<Input name={prop} {label} type="number" on:change={handleChange} />
+		{/if}
+	{/each}
 </main>
---
+++ src/Input.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	const dispatch = createEventDispatcher();
+
+	type InputType = "select" | "button" | "number";
+
+	export let label: string;
+	export let name: string;
+	export let type: InputType;
+	export let values: any[];
+
+	export let value: any;
+
+	function onChange(e) {
+		value = e.target.value;
+		dispatch("change", {
+			name: name,
+			value: value,
+		});
+	}
+</script>
+
+<style>
+	div {
+		display: grid;
+		grid-template-columns: 1fr 3fr;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	.button-group {
+		display: flex;
+		gap: 0;
+	}
+	.button-group > button {
+		flex: 1;
+		margin: 0;
+	}
+</style>
+
+<div>
+	<label for={name}>{label}</label>
+
+	{#if type == 'select'}
+		<select {name} on:input={onChange}>
+			{#each values as value}
+				<option {value}>{value}</option>
+			{/each}
+		</select>
+	{:else if type == 'button'}
+		<div class="button-group">
+			{#each values as value}
+				<button on:click={onChange} {value} {name}>{value}</button>
+			{/each}
+		</div>
+	{:else if type == 'number'}<input on:input={onChange} type="number" />{/if}
+</div>
